8cdfc28559e81d048732f543c5e8356e8440e1e0
Patricia C <49651427+paaatt00@users.noreply.github.com>
2021-05-09 18:32:39 +0200

    Delete Unit1/deliverie directory

--- Unit1/deliverie/perfect_num.py
+++
@@ -1,8 +0,0 @@
-def isPerfect(num):
-    n = 0
-    for i in range(1, num):
-        if num % i == 0:
-            n = n + i
-    return n == num
-
-print(isPerfect(6))
--- Unit1/deliverie/prime_num.py
+++
@@ -1,8 +0,0 @@
-def isPrime(num):
-    b = True
-    for i in range(2, num):
-        if num % i == 0:
-            b = False
-    return b
-
-print(isPrime(12))
